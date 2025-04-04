bba69f4a0c22a1dec3475e3dd895a78e8a11b990
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-04-05 01:37:55 +0300

    add upload report functinality

--- server/src/route/upload.ts
+++ server/src/route/upload.ts
@@ -3,7 +3,7 @@ import upload from "../middelware/multermiddelware.js";
 import { uploadFiles, uploadImage } from "../controller/handleUpload.js";
 const router = express.Router();
 
-router.post("/", upload.single("file"), uploadFiles);
-router.post("/image", upload.array("images"), uploadImage);
+router.post("/:id", upload.single("file"), uploadFiles);
+router.post("/report:id", upload.array("images"), uploadImage);
 
 export default router;
